65a5166338ed5b481d67be5e7192764ade3775c5
WashingtonApolinario <dekontruccion@gmail>
2018-11-27 10:17:15 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var datosJson = '[{ "Name" : "Alfreds Futterkiste", "City" : "Berlin", "Country" : "Germany" }, { "Name" : "Berglunds snabbköp", "City" : "Luleå", "Country" : "Sweden" }, { "Name" : "Centro comercial Moctezuma", "City" : "México D.F.", "Country" : "Mexico" }, { "Name" : "Ernst Handel", "City" : "Graz", "Country" : "Austria" }, { "Name" : "FISSA Fabrica Inter. Salchichas S.A.", "City" : "Madrid", "Country" : "Spain" }, { "Name" : "Galería del gastrónomo", "City" : "Barcelona", "Country" : "Spain" }, { "Name" : "Island Trading", "City" : "Cowes", "Country" : "UK" }, { "Name" : "Königlich Essen", "City" : "Brandenburg", "Country" : "Germany" }, { "Name" : "Laughing Bacchus Wine Cellars", "City" : "Vancouver", "Country" : "Canada" }, { "Name" : "Magazzini Alimentari Riuniti", "City" : "Bergamo", "Country" : "Italy" }, { "Name" : "North/South", "City" : "London", "Country" : "UK" }, { "Name" : "Paris spécialités", "City" : "Paris", "Country" : "France" }, { "Name" : "Rattlesnake Canyon Grocery", "City" : "Albuquerque", "Country" : "USA" }, { "Name" : "Simons bistro", "City" : "København", "Country" : "Denmark" }, { "Name" : "The Big Cheese", "City" : "Portland", "Country" : "USA" }, { "Name" : "Vaffeljernet", "City" : "Århus", "Country" : "Denmark" }, { "Name" : "Wolski Zajazd", "City" : "Warszawa", "Country" : "Poland" }]';
+
+
+
+  alert("funcionando-2.....");
+    var personas = JSON.parse(datosJson);
+    console.log(personas);
+  
+    var tabla="<tr> <th>NOMBRE</th> <th>CIUDAD</th> <th>PAÍS</th> </tr>";
+
+    for (var i = 0; i < personas.length; i++) {
+    tabla += ("<tr><td>" + personas[i].Name + "</td> <td>" +personas[i].City +"</td> <td>" +personas[i].Country +"</td></tr>");
+    }
+  
+  $("#idData").append(tabla);
+  
+
+
+
+
+
+
+
+
+/*$.each(personas.length, function (i, item){
+    
+    tabla += ("<tr><td>" + personas[i].Name + "</td> <td>" +personas[i].City +"</td> <td>" +personas[i].Country +"</td></tr>");
+  });*/
+  
